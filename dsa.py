1187632734da9ceb49258bc1400a151dbc879050
Brian Archibald <brianallenarchibald@gmail.com>
2021-09-12 17:41:22 -0500

    brute force linear

--- dsa.py
+++ dsa.py
@@ -32,3 +32,20 @@ test = {
     },
     'output': 3
 }
+
+## Brute Force
+def count_rotations_linear(nums):
+    position = 0
+    # What is the intial value of position?
+    
+    while position < len(nums):  
+        # When should the loop be terminated?
+        
+        # Success criteria: check whether the number at the current position is smaller than the one before it
+        if position > 0 and nums[position] < nums[position - 1]:   # How to perform the check?
+            return position
+        
+        # Move to the next position
+        position += 1
+    
+    return 0   
