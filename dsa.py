2a74bc576ad3819734bd314d2c4f65f47254bea1
BrianArchibald <brianallenarchibald@gmail.com>
2021-09-15 13:36:00 -0500

    binary

--- dsa.py
+++ dsa.py
@@ -49,3 +49,22 @@ def count_rotations_linear(nums):
         position += 1
     
     return 0   
+
+##  binary serach solution
+def locate_card(cards, query):
+    lo, hi = 0, len(cards) - 1
+    
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        mid_number = cards[mid]
+        
+        print("lo:", lo, ", hi:", hi, ", mid:", mid, ", mid_number:", mid_number)
+        
+        if mid_number == query:
+            return mid
+        elif mid_number < query:
+            hi = mid - 1  
+        elif mid_number > query:
+            lo = mid + 1
+    
+    return -1
